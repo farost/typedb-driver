1e364c694fb61989f709e3d995aea37c99dd671f
Georgii Novoselov <georgii@typedb.com>
2024-11-19 18:43:04 +0000

    Prepare connection and transaction open benches (connection is probably wrong idk)

---
+++ rust/tests/benchmark/connection_open.rs
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#![deny(unused_must_use)]
+
+use std::{
+    ffi::c_int,
+    fs::File,
+    path::Path,
+};
+use criterion::{Criterion, criterion_group, criterion_main, SamplingMode};
+use criterion::profiler::Profiler;
+use pprof::ProfilerGuard;
+use typedb_driver::TypeDBDriver;
+
+fn create_driver() {
+    async_std::task::block_on(async {
+        TypeDBDriver::new_core(TypeDBDriver::DEFAULT_ADDRESS).await.expect("Expected driver");
+    })
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("test connection open");
+    // group.sample_size(1000);
+    // group.measurement_time(Duration::from_secs(200));
+    group.sampling_mode(SamplingMode::Linear);
+
+    group.bench_function("connection_open", |b| {
+        b.iter(|| {
+            create_driver()
+        });
+    });
+}
+
+pub struct FlamegraphProfiler<'a> {
+    frequency: c_int,
+    active_profiler: Option<ProfilerGuard<'a>>,
+}
+
+impl<'a> FlamegraphProfiler<'a> {
+    #[allow(dead_code)]
+    pub fn new(frequency: c_int) -> Self {
+        Self { frequency, active_profiler: None }
+    }
+}
+
+impl<'a> Profiler for FlamegraphProfiler<'a> {
+    fn start_profiling(&mut self, _benchmark_id: &str, _benchmark_dir: &Path) {
+        self.active_profiler = Some(ProfilerGuard::new(self.frequency).unwrap());
+    }
+
+    fn stop_profiling(&mut self, _benchmark_id: &str, benchmark_dir: &Path) {
+        std::fs::create_dir_all(benchmark_dir).unwrap();
+        let flamegraph_path = benchmark_dir.join("flamegraph.svg");
+        let flamegraph_file = File::create(flamegraph_path).expect("File system error while creating flamegraph.svg");
+        if let Some(profiler) = self.active_profiler.take() {
+            profiler.report().build().unwrap().flamegraph(flamegraph_file).expect("Error writing flamegraph");
+        }
+    }
+}
+
+fn profiled() -> Criterion {
+    Criterion::default().with_profiler(FlamegraphProfiler::new(10))
+}
+
+// criterion_group!(
+//     name = benches;
+//     config= profiled();
+//     targets = criterion_benchmark
+// );
+
+// TODO: disable profiling when running on mac, since pprof seems to crash sometimes?
+criterion_group!(benchmark, criterion_benchmark);
+criterion_main!(benchmark);
---
+++ rust/tests/benchmark/transaction_open_multithreaded.rs
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#![deny(unused_must_use)]
+
+use std::{
+    array,
+    sync::{Arc, RwLock},
+    thread,
+    thread::{sleep, JoinHandle},
+    time::{Duration, Instant},
+};
+
+use typedb_driver::{TransactionType, TypeDBDriver};
+
+const DB_NAME: &'static str = "benchmark";
+
+fn prepare() -> TypeDBDriver {
+    async_std::task::block_on(async {
+        let driver = TypeDBDriver::new_core(TypeDBDriver::DEFAULT_ADDRESS).await.expect("Expected driver");
+        if driver.databases().contains(DB_NAME).await.unwrap() {
+            driver.databases().get(DB_NAME).await.expect("Expect database get").delete().await.expect("Expected database delete");
+        }
+        driver.databases().create(DB_NAME).await.expect("Expect database create");
+        driver
+    })
+}
+
+fn open_transaction(driver: &TypeDBDriver) {
+    async_std::task::block_on(async {
+        driver.transaction(DB_NAME, TransactionType::Read).await.expect("Expected transaction");
+    })
+}
+
+fn multi_threaded_inserts() {
+    let driver = prepare();
+
+    const NUM_THREADS: usize = 32;
+    const INTERNAL_ITERS: usize = 1000;
+    let start_signal_rw_lock = Arc::new(RwLock::new(()));
+    let write_guard = start_signal_rw_lock.write().unwrap();
+    let join_handles: [JoinHandle<()>; NUM_THREADS] = array::from_fn(|_| {
+        let driver_cloned = driver.clone();
+        let rw_lock_cloned = start_signal_rw_lock.clone();
+        thread::spawn(move || {
+            drop(rw_lock_cloned.read().unwrap());
+            for _ in 0..INTERNAL_ITERS {
+                open_transaction(&driver_cloned)
+            }
+        })
+    });
+    println!("Sleeping 1s before starting threads");
+    sleep(Duration::from_secs(1));
+    println!("Start!");
+    let start = Instant::now();
+    drop(write_guard); // Start
+    for join_handle in join_handles {
+        join_handle.join().unwrap()
+    }
+    let time_taken_ms = start.elapsed().as_millis();
+    println!(
+        "{NUM_THREADS} threads * {INTERNAL_ITERS} iters took: {time_taken_ms} ms = {} transactions/s",
+        (NUM_THREADS * INTERNAL_ITERS * 1000) / time_taken_ms as usize
+    );
+}
+
+fn main() {
+    multi_threaded_inserts();
+}
