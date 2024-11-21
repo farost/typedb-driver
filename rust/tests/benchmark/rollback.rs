ee47c4dcfd666b5a9c886efd21c0b468dec5e2e0
Georgii Novoselov <georgii@typedb.com>
2024-11-21 11:42:26 +0000

    Add rollback tests

---
+++ rust/tests/benchmark/rollback.rs
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#![deny(unused_must_use)]
+
+use std::{
+    ffi::c_int,
+    fs::File,
+    path::Path,
+};
+use criterion::{Criterion, criterion_group, criterion_main, SamplingMode, Throughput};
+use criterion::profiler::Profiler;
+use pprof::ProfilerGuard;
+use typedb_driver::{Connection, DatabaseManager, Session, SessionType, Transaction, TransactionType};
+
+const DB_NAME: &'static str = "benchmark";
+
+fn prepare_session() -> Session {
+    async_std::task::block_on(async {
+        let connection = Connection::new_core("127.0.0.1:1730").expect("Expected driver");
+        let db_manager = DatabaseManager::new(connection.clone());
+        if db_manager.contains(DB_NAME).await.unwrap() {
+            db_manager.get(DB_NAME).await.expect("Expected database get").delete().await.expect("Expected database delete");
+        }
+        db_manager.create(DB_NAME).await.expect("Expected database create");
+        Session::new(db_manager.get(DB_NAME).await.expect("Expected database"), SessionType::Schema).await.expect("Expected session")
+    })
+}
+
+fn prepare_transaction(session: &Session) -> Transaction<'_> {
+    async_std::task::block_on(async {
+        session.transaction(TransactionType::Read).await.expect("Expected transaction")
+    })
+}
+
+fn rollback(transaction: &Transaction<'_>) {
+    async_std::task::block_on(async {
+        transaction.rollback().await.expect("Expected rollback result");
+    })
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("test rollback");
+    // group.sample_size(1000);
+    // group.measurement_time(Duration::from_secs(200));
+    group.sampling_mode(SamplingMode::Linear);
+
+    let session = prepare_session();
+    let transaction = prepare_transaction(&session);
+
+    group.throughput(Throughput::Elements(1)); // calls/sec
+    group.bench_function("rollback", |b| {
+        b.iter(|| {
+            rollback(&transaction)
+        });
+    });
+    group.finish();
+}
+
+pub struct FlamegraphProfiler<'a> {
+    frequency: c_int,
+    active_profiler: Option<ProfilerGuard<'a>>,
+}
+
+impl<'a> FlamegraphProfiler<'a> {
+    #[allow(dead_code)]
+    pub fn new(frequency: c_int) -> Self {
+        Self { frequency, active_profiler: None }
+    }
+}
+
+impl<'a> Profiler for FlamegraphProfiler<'a> {
+    fn start_profiling(&mut self, _benchmark_id: &str, _benchmark_dir: &Path) {
+        self.active_profiler = Some(ProfilerGuard::new(self.frequency).unwrap());
+    }
+
+    fn stop_profiling(&mut self, _benchmark_id: &str, benchmark_dir: &Path) {
+        std::fs::create_dir_all(benchmark_dir).unwrap();
+        let flamegraph_path = benchmark_dir.join("flamegraph.svg");
+        let flamegraph_file = File::create(flamegraph_path).expect("File system error while creating flamegraph.svg");
+        if let Some(profiler) = self.active_profiler.take() {
+            profiler.report().build().unwrap().flamegraph(flamegraph_file).expect("Error writing flamegraph");
+        }
+    }
+}
+
+fn profiled() -> Criterion {
+    Criterion::default().with_profiler(FlamegraphProfiler::new(10))
+}
+
+// criterion_group!(
+//     name = benches;
+//     config= profiled();
+//     targets = criterion_benchmark
+// );
+
+// TODO: disable profiling when running on mac, since pprof seems to crash sometimes?
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ rust/tests/benchmark/rollback_multithreaded.rs
@@ -0,0 +1,99 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#![deny(unused_must_use)]
+
+use std::{
+    array,
+    sync::{Arc, RwLock},
+    thread,
+    thread::{sleep, JoinHandle},
+    time::{Duration, Instant},
+};use std::rc::Rc;
+
+
+use typedb_driver::{Connection, DatabaseManager, Session, SessionType, Transaction, TransactionType};
+
+const DB_NAME: &'static str = "benchmark";
+
+fn prepare_session() -> Session {
+    async_std::task::block_on(async {
+        let connection = Connection::new_core("127.0.0.1:1730").expect("Expected driver");
+        let db_manager = DatabaseManager::new(connection.clone());
+        if db_manager.contains(DB_NAME).await.unwrap() {
+            db_manager.get(DB_NAME).await.expect("Expected database get").delete().await.expect("Expected database delete");
+        }
+        db_manager.create(DB_NAME).await.expect("Expected database create");
+        Session::new(db_manager.get(DB_NAME).await.expect("Expected database"), SessionType::Schema).await.expect("Expected session")
+    })
+}
+
+fn prepare_transaction(session: &Session) -> Transaction<'_> {
+    async_std::task::block_on(async {
+        session.transaction(TransactionType::Read).await.expect("Expected transaction")
+    })
+}
+
+fn rollback(transaction: &Transaction<'_>) {
+    async_std::task::block_on(async {
+        transaction.rollback().await.expect("Expected rollback result");
+    })
+}
+
+fn multi_threaded_inserts() {
+    let session = prepare_session();
+    let transaction: Arc<Transaction<'static>> = Arc::new(
+    unsafe {
+        std::mem::transmute(prepare_transaction(&session))
+    });
+
+    const NUM_THREADS: usize = 16;
+    const INTERNAL_ITERS: usize = 10000;
+    let start_signal_rw_lock = Arc::new(RwLock::new(()));
+    let write_guard = start_signal_rw_lock.write().unwrap();
+    let join_handles: [JoinHandle<()>; NUM_THREADS] = array::from_fn(|_| {
+        let transaction_cloned = transaction.clone();
+        let rw_lock_cloned = start_signal_rw_lock.clone();
+        thread::spawn(move || {
+            drop(rw_lock_cloned.read().unwrap());
+            for _ in 0..INTERNAL_ITERS {
+                rollback(transaction_cloned.as_ref())
+            }
+        })
+    });
+    println!("Sleeping 1s before starting threads");
+    sleep(Duration::from_secs(1));
+    println!("Start!");
+    let start = Instant::now();
+    drop(write_guard); // Start
+    for join_handle in join_handles {
+        join_handle.join().unwrap()
+    }
+    let time_taken_ms = start.elapsed().as_millis();
+    println!(
+        "{NUM_THREADS} threads * {INTERNAL_ITERS} iters took: {time_taken_ms} ms = {} transactions/s",
+        (NUM_THREADS * INTERNAL_ITERS * 1000) / time_taken_ms as usize
+    );
+
+    drop(transaction);
+}
+
+fn main() {
+    multi_threaded_inserts();
+}
